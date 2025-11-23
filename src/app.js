ae50865953128542b90eb84a8989c4a73156cc34
Matheus Kauãc <mkmateus.dev@gmail.com>
2025-11-23 14:42:50 -0300

    feat: implementa model, service e controller da classe Comments

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ import "dotenv/config";
 import cors from "cors";
 import express from "express";
 import { sequelize } from "./models/index.js";
-import {userController} from "./controllers/index.js"
+import { userController } from "./controllers/index.js";
 const app = express();
 app.set("trust proxy", true);
 
 var corsOptions = {
   origin: ["http://example.com", "*"],
-  optionsSuccessStatus: 200, 
+  optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 
@@ -22,15 +22,17 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res)=>{
-    res.status(200).json({message: "Server Ok!"})
-})
-app.use("/user", userController)
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Server Ok!" });
+});
 
-sequelize.sync({force: true}).then(async () => {
-    app.listen(8080,()=>{
-        console.log(`Server is running in http://localhost:8080`);
-    })
-})
+app.use("/user", userController);
+app.use("/comments", commentController);
+
+sequelize.sync({ force: true }).then(async () => {
+  app.listen(8080, () => {
+    console.log(`Server is running in http://localhost:8080`);
+  });
+});
 
 export default app;
