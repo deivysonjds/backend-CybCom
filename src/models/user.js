e3a6581d5bc90660d46adf8d3719fb5a3a3d91fd
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-12-02 23:48:54 -0300

    added: cors

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,5 @@
 import argon2, { argon2id } from "argon2"
 import "dotenv/config"
-import { validate } from "uuid";
 
 const getUserModel = (sequelize, { DataTypes }) => {
   const User = sequelize.define("user", {
@@ -69,7 +68,6 @@ const getUserModel = (sequelize, { DataTypes }) => {
       otherKey: 'followerId',      
     });
 
-    // Quem este usuário segue
     User.belongsToMany(models.User, {
       through: models.Follower,
       as: 'following',
